0816533115796b0d0d0bdfd27798f219a28fcea6
Ronny Amberg <ronny.amberg@gmail.com>
2019-05-05 14:22:10 +0200

    Create highlight-current-row.vb

---
+++ highlight-current-row.vb
@@ -0,0 +1,11 @@
+' (1) Create a formula "Formulas > Define Name" which is called CurrentRow and Refers to "=1"
+' (2) Create a conditional formatting "Home > C.F." which evaluates the formular "=ROW(A1)=CurrentRow"
+' (3) Create the following macro
+' (4) Save Excel File as xlsm (w/ macros)
+
+Private Sub Worksheet_SelectionChange(ByVal Target As Range)
+    With ThisWorkbook.Names("CurrentRow")
+        .Name = "CurrentRow"
+        .RefersToR1C1 = "=" & ActiveCell.Row
+    End With
+End Sub
